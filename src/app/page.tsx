ba138598b47e4676aa2144f325abad5c2bee6f46
かがみ <seki@kagami.me>
2024-04-06 01:30:11 +0800

    feat: title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,4 @@
-import {
-  ArrowUpRight,
-  Menu,
-  Package2,
-  Search
-} from "lucide-react";
+import { ArrowUpRight, Menu, Search } from "lucide-react";
 import Link from "next/link";
 
 import { Badge } from "~/components/ui/badge";
@@ -26,47 +21,31 @@ import {
   TableRow,
 } from "~/components/ui/table";
 
+import { SiteTitle } from "~/components/site-title";
+
 export default function Dashboard() {
   return (
     <div className="flex min-h-screen w-full flex-col">
       <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
         <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+          <SiteTitle className="flex items-center gap-2 text-4xl font-semibold" />
           <Link
             href="#"
-            className="flex items-center gap-2 text-lg font-semibold md:text-base"
+            className="whitespace-nowrap text-foreground transition-colors hover:text-foreground"
           >
-            <Package2 className="h-6 w-6" />
-            <span className="sr-only">Acme Inc</span>
+            首页
           </Link>
           <Link
             href="#"
-            className="text-foreground transition-colors hover:text-foreground"
+            className="whitespace-nowrap text-muted-foreground transition-colors hover:text-foreground"
           >
-            Dashboard
+            文章
           </Link>
           <Link
             href="#"
-            className="text-muted-foreground transition-colors hover:text-foreground"
+            className="whitespace-nowrap text-muted-foreground transition-colors hover:text-foreground"
           >
-            Orders
-          </Link>
-          <Link
-            href="#"
-            className="text-muted-foreground transition-colors hover:text-foreground"
-          >
-            Products
-          </Link>
-          <Link
-            href="#"
-            className="text-muted-foreground transition-colors hover:text-foreground"
-          >
-            Customers
-          </Link>
-          <Link
-            href="#"
-            className="text-muted-foreground transition-colors hover:text-foreground"
-          >
-            Analytics
+            关于
           </Link>
         </nav>
         <Sheet>
@@ -82,39 +61,21 @@ export default function Dashboard() {
           </SheetTrigger>
           <SheetContent side="left">
             <nav className="grid gap-6 text-lg font-medium">
-              <Link
-                href="#"
-                className="flex items-center gap-2 text-lg font-semibold"
-              >
-                <Package2 className="h-6 w-6" />
-                <span className="sr-only">Acme Inc</span>
-              </Link>
+              <SiteTitle className="flex items-center gap-2 text-2xl font-semibold" />
               <Link href="#" className="hover:text-foreground">
-                Dashboard
+                首页
               </Link>
               <Link
                 href="#"
                 className="text-muted-foreground hover:text-foreground"
               >
-                Orders
+                文章
               </Link>
               <Link
                 href="#"
                 className="text-muted-foreground hover:text-foreground"
               >
-                Products
-              </Link>
-              <Link
-                href="#"
-                className="text-muted-foreground hover:text-foreground"
-              >
-                Customers
-              </Link>
-              <Link
-                href="#"
-                className="text-muted-foreground hover:text-foreground"
-              >
-                Analytics
+                关于
               </Link>
             </nav>
           </SheetContent>
---
+++ src/components/site-title.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import { Logo } from "./logo";
+import { type FC } from "react";
+
+interface SiteTitleProps {
+  className?: string;
+}
+
+export const SiteTitle: FC<SiteTitleProps> = ({ className }) => (
+  <Link href="#" className={className}>
+    <Logo />
+    <span className="sr-only">アトリエにじか</span>
+  </Link>
+);
