b8d59fce9a14693aa44855addbfac576eb9eda81
かがみ <seki@kagami.me>
2024-04-21 14:55:32 +0800

    feat: suspense for use search params

--- src/app/search/search.tsx
+++ src/app/search/search.tsx
@@ -9,6 +9,7 @@ import { Button } from "~/components/ui/button";
 import { Card, CardContent, CardHeader } from "~/components/ui/card";
 import { Input } from "~/components/ui/input";
 import { handleSearch } from "./actions";
+import { Suspense } from "react";
 
 const SearchButton = () => {
   const { pending } = useFormStatus();
@@ -21,22 +22,29 @@ const SearchButton = () => {
   );
 };
 
-export const Search = () => {
+const SearchInput = () => {
   const searchParams = useSearchParams();
+  return (
+    <Input
+      name="q"
+      placeholder="关键词"
+      defaultValue={searchParams.get("q") ?? ""}
+    />
+  );
+};
 
+export const Search = () => {
   const [formState, action] = useFormState(handleSearch, { result: null });
 
-  console.log(formState.result)
+  console.log(formState.result);
 
   return (
     <div className="flex flex-col gap-8">
       <form action={action}>
         <div className="flex gap-4">
-          <Input
-            name="q"
-            placeholder="关键词"
-            defaultValue={searchParams.get("q") ?? ""}
-          />
+          <Suspense>
+            <SearchInput />
+          </Suspense>
           <SearchButton />
         </div>
       </form>
@@ -45,14 +53,15 @@ export const Search = () => {
           <CardHeader>搜索结果</CardHeader>
           <CardContent>
             <div className="flex flex-col gap-4">
-                {formState.result!.map((item: SearchResult | Post) => (
-                  <div key={item._id as string}>
-                    <div className="text-sm">
-                      有 {Object.keys((item as SearchResult).match).length} 个匹配结果
-                    </div>
-                    <PostItem {...(item as Post)} />
+              {formState.result!.map((item: SearchResult | Post) => (
+                <div key={item._id as string}>
+                  <div className="text-sm">
+                    有 {Object.keys((item as SearchResult).match).length}{" "}
+                    个匹配结果
                   </div>
-                ))}
+                  <PostItem {...(item as Post)} />
+                </div>
+              ))}
             </div>
           </CardContent>
         </Card>
