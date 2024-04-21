f0b09ebed4649537bf8e5933513e3bae8075077e
かがみ <seki@kagami.me>
2024-04-21 10:25:48 +0800

    feat: ensure time refresh

--- src/components/local-time.tsx
+++ src/components/local-time.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useMounted } from "~/hooks/use-mounted";
 import { cn } from "~/lib/utils";
 
 interface LocalTimeProps {
@@ -8,11 +9,13 @@ interface LocalTimeProps {
 }
 
 export const LocalTime = ({ date, className }: LocalTimeProps) => {
+  const mounted = useMounted()
   return (
     <time
       dateTime={date}
       className={cn(className, "mb-2 block text-xs")}
       suppressHydrationWarning
+      key={mounted ? 'client' : 'server'}
     >
       {new Intl.DateTimeFormat("zh-CN", {
         year: "numeric",
