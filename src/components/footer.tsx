bac3b1d63b5336e614f23b6e7d7a83248b4f4ed8
かがみ <seki@kagami.me>
2024-04-21 14:48:40 +0800

    fix: typo

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   const nowYear = new Date().getFullYear()
   return (
     <footer className="p-4 md:p-8 border-t">
-      <p>©{nowYear === 2024 ? nowYear : `2024-${nowYear}`} アティエ<span className="text-primary">にじか</span> feat.<span className="text-[#0099FF]">かがみ</span>， 内容基于 <a href="https://creativecommons.org/licenses/by-sa/4.0/legalcode">CC-BY-SA 4.0</a> 授权</p>
+      <p>©{nowYear === 2024 ? nowYear : `2024-${nowYear}`} アトリエ<span className="text-primary">にじか</span> feat.<span className="text-[#0099FF]">かがみ</span>， 内容基于 <a href="https://creativecommons.org/licenses/by-sa/4.0/legalcode">CC-BY-SA 4.0</a> 授权</p>
     </footer>
   )
 }
