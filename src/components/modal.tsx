ec6cecc7a54819c1adec87cb25e5efd35d8b0ce6
かがみ <seki@kagami.me>
2024-04-23 00:37:22 +0800

    feat: parallel rendering for search

---
+++ src/components/modal.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+import { Content } from "@radix-ui/react-dialog"
+import { type PropsWithChildren, type FC } from "react"
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogOverlay,
+  DialogPortal,
+} from "~/components/ui/dialog"
+
+export const SearchModal: FC<PropsWithChildren> = ({ children }) => {
+  const router = useRouter()
+  return (
+    <Dialog defaultOpen onOpenChange={() => router.back()}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>搜索</DialogTitle>
+        </DialogHeader>
+        {children}
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+// a modal for skeleton loading, removing the close button provided by DialogContent
+export const SkeletonModal: FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <Dialog defaultOpen>
+      <DialogPortal>
+        <DialogOverlay />
+        <Content className="fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg">
+          {children}
+        </Content>
+      </DialogPortal>
+    </Dialog>
+  )
+}
