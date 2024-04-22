0a60541425b7f63829387a735807aec5e04de997
かがみ <seki@kagami.me>
2024-04-22 14:50:30 +0800

    feat: avoid layout refresh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { allPosts } from "contentlayer/generated";
 import { compareDesc } from "date-fns";
-import { CommonLayout } from "~/components/common-layout";
 
 import { PostItem } from "../components/post-item";
 
@@ -9,12 +8,10 @@ export default function AppHome() {
     compareDesc(new Date(a.date), new Date(b.date)),
   );
   return (
-    <CommonLayout>
-      <div className="flex flex-col gap-4">
-        {posts.map((post) => (
-          <PostItem key={post._id} {...post} />
-        ))}
-      </div>
-    </CommonLayout>
+    <div className="flex flex-col gap-4">
+      {posts.map((post) => (
+        <PostItem key={post._id} {...post} />
+      ))}
+    </div>
   );
 }
