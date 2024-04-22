d263b136189f7ec2550acdf81ce95db726377626
かがみ <seki@kagami.me>
2024-04-23 01:09:02 +0800

    feat: add playwright tests

--- src/components/site-title.tsx
+++ src/components/site-title.tsx
@@ -8,7 +8,9 @@ interface SiteTitleProps {
 
 export const SiteTitle: FC<SiteTitleProps> = ({ className }) => (
   <Link href="/" className={className}>
-    <Logo />
-    <span className="sr-only">アトリエにじか</span>
+    <h1>
+      <Logo />
+      <span className="sr-only">アトリエにじか</span>
+    </h1>
   </Link>
 )
---
+++ tests/example.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from "@playwright/test"
+
+test("has title", async ({ page }) => {
+  await page.goto("/")
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/アトリエにじか/)
+
+  await expect(page.locator("header")).toBeVisible()
+  await expect(page.locator("header").getByText("アトリエにじか")).toBeVisible()
+
+  await expect(page.locator("footer")).toBeVisible()
+  await expect(page.locator("footer").getByText("アトリエにじか")).toBeVisible()
+})
+
+test("about page", async ({ page }) => {
+  await page.goto("/about")
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/アトリエにじか/)
+  await expect(page).toHaveTitle(/关于/)
+
+  await expect(page.locator("header")).toBeVisible()
+  await expect(page.locator("header").getByText("アトリエにじか")).toBeVisible()
+
+  await expect(page.locator("footer")).toBeVisible()
+  await expect(page.locator("footer").getByText("アトリエにじか")).toBeVisible()
+})
