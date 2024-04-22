5c770d23d486ad8e92f4f312cda202dc977cd985
かがみ <seki@kagami.me>
2024-04-22 09:39:18 +0800

    feat: add commit hash

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,6 +3,7 @@ export const Footer = () => {
   return (
     <footer className="p-4 md:p-8 border-t">
       <p>©{nowYear === 2024 ? nowYear : `2024-${nowYear}`} アトリエ<span className="text-primary">にじか</span> feat.<span className="text-[#0099FF]">かがみ</span>， 内容基于 <a href="https://creativecommons.org/licenses/by-sa/4.0/legalcode">CC-BY-SA 4.0</a> 授权</p>
+      {process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA && <p>版本：{process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA}-{process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF}</p> }
     </footer>
   )
 }
