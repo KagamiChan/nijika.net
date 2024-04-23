b9f6d2b7e049922b2a705eee620aa24f3e6b0b50
かがみ <seki@kagami.me>
2024-04-23 23:11:50 +0800

    fix: close side nav when clicked

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,9 @@
-import { Menu } from 'lucide-react'
-
 import { NavItem } from './nav-item'
 import { SearchLink } from './search-link'
+import { SideNav } from './side-nav'
 import { ThemeChooser } from './theme-chooser'
 
 import { SiteTitle } from '~/components/site-title'
-import { Button } from '~/components/ui/button'
-import { Sheet, SheetContent, SheetTrigger } from '~/components/ui/sheet'
 
 export const Header = () => {
   return (
@@ -16,21 +13,7 @@ export const Header = () => {
         <NavItem href="/">首页</NavItem>
         <NavItem href="/about">关于</NavItem>
       </nav>
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button variant="ghost" size="icon" className="shrink-0 md:hidden">
-            <Menu className="h-5 w-5" />
-            <span className="sr-only">打开导航菜单</span>
-          </Button>
-        </SheetTrigger>
-        <SheetContent side="left">
-          <nav className="grid gap-6 text-lg font-medium">
-            <SiteTitle className="flex items-center gap-2 text-2xl font-semibold" />
-            <NavItem href="/">首页</NavItem>
-            <NavItem href="/about">关于</NavItem>
-          </nav>
-        </SheetContent>
-      </Sheet>
+      <SideNav />
       <div className="flex w-full items-center justify-end gap-4 md:ml-auto md:gap-2 lg:gap-4">
         <SearchLink />
         <ThemeChooser />
---
+++ src/components/side-nav.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { Menu } from 'lucide-react'
+
+import { NavItem } from './nav-item'
+
+import { SiteTitle } from '~/components/site-title'
+import { Button } from '~/components/ui/button'
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from '~/components/ui/sheet'
+
+export const SideNav = () => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="ghost" size="icon" className="shrink-0 md:hidden">
+          <Menu className="h-5 w-5" />
+          <span className="sr-only">打开导航菜单</span>
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left">
+        <nav className="grid gap-6 text-lg font-medium">
+          <SiteTitle className="flex items-center gap-2 text-2xl font-semibold" />
+          <SheetClose asChild>
+            <NavItem href="/">首页</NavItem>
+          </SheetClose>
+          <SheetClose asChild>
+            <NavItem href="/about">关于</NavItem>
+          </SheetClose>
+        </nav>
+      </SheetContent>
+    </Sheet>
+  )
+}
